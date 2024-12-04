1ded106dd5fa62517d1415077140fcf45a5fa69e
naixin9393 <naicinchen@gmail.com>
2024-12-04 17:25:23 +0000

    Add 3D rubik cube

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 import * as THREE from "three";
-import TWEEN from 'https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.5.0/dist/tween.esm.js'
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+import TWEEN from "https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.5.0/dist/tween.esm.js";
+import RubikCube from "./rubik-cube.js";
+
 let scene, renderer;
 let camera;
 let objetos = [];
@@ -8,22 +12,36 @@ init();
 loop();
 
 function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.set(0, 0, 10);
-  
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    setScene();
+    setControls(camera, renderer.domElement);
+
+    const rubikCube = new RubikCube();
+    rubikCube.addToScene(scene);
 }
 
 function loop() {
     requestAnimationFrame(loop);
     // TWEEN.update();
     renderer.render(scene, camera);
+}
+
+function setScene() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.set(4, 2.5, 4);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function setControls(camera, dom) {
+    const controls = new OrbitControls(camera, dom);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
 }
---
+++ public/rubik-cube.js
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+
+export default class RubikCube {
+    cubies = [];
+
+    constructor() {
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                for (let z = -1; z <= 1; z++) {
+                    const geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
+
+                    const materials = [
+                        new THREE.MeshBasicMaterial({ color: colors.right }), // Right
+                        new THREE.MeshBasicMaterial({ color: colors.left }), // Left
+                        new THREE.MeshBasicMaterial({ color: colors.top }), // Top
+                        new THREE.MeshBasicMaterial({ color: colors.bottom }), // Bottom
+                        new THREE.MeshBasicMaterial({ color: colors.front }), // Front
+                        new THREE.MeshBasicMaterial({ color: colors.back }), // Back
+                    ];
+
+                    const cubie = new THREE.Mesh(geometry, materials);
+
+                    cubie.position.set(x, y, z);
+                    this.cubies.push(cubie);
+                }
+            }
+        }
+    }
+
+    addToScene(scene) {
+        for (let cubie of this.cubies) {
+            scene.add(cubie);
+        }
+    }
+}
+
+const colors = {
+    front: 0xff4444, // Red
+    back: 0xff8000, // Orange
+    left: 0x0000ff, // Blue
+    right: 0x00ff00, // Green
+    top: 0xffff33, // Yellow
+    bottom: 0xffffff, // White
+};
